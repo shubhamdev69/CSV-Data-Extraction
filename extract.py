6846b9020c6086342e3d59d200cfa460d86e2cb6
shubhamdev69 <shubhamkumarsingh5901@gmail.com>
2023-07-24 10:29:22 +0530

    Added some comments in the script

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@
 import re
 import csv
 
+#Opening two files, one for the event click, other for render and writing columns headers into them
 clickData = open("clickData.csv", 'w')
 clickData.write("event,adId,campaignID,xForwardedFor,stateID,cityID" + '\n') 
 renderData = open("renderData.csv", 'w')
